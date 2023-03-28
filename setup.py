572c0f526255355139659d84f36ce2ce320baa55
Savino Jossi <3669673+sjossi@users.noreply.github.com>
2023-03-28 09:22:11 -0400

    add setup.py to allow for cli installation

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="scare",
+    version="0.3.0",
+    description="scare is a multi-arch assembly REPL and emulator for your command line.",
+    author="netspooky",
+    license="GPLv2",
+    license_files=["LICENSE.md"],
+    classifiers=["License :: OSI Approved :: GNU General Public License v2 (GPLv2)"],
+    requires=["unicorn", "keystone", "capstone"],
+    py_modules=["scare"],
+    entry_points={
+        "console_scripts": [
+            "scare = scare",
+        ]
+    },
+)
